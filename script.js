4fb337db737227eeda355b921e38a09c9d8f4431
tmhbstaines <tmhbstaine@gmail.com>
2025-05-10 17:20:16 +0200

    Ajout des fichiers du site
    Ajout des fichiers du site

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const video = document.getElementById('videoBackground');
+    if (video) {
+      video.playbackRate = 0.7; 
+    }
+  });
+  
